dff703f252fe4964b612c91f9a41526fe905520d
Diogo Correia <me@diogotc.com>
2026-01-20 01:35:26 +0100

    refactor(gdrawings): use native fetch api instead of request package

--- src/projects/gdrawings/gdrawings.js
+++ src/projects/gdrawings/gdrawings.js
@@ -1,27 +1,31 @@
-import request from 'request';
+const handleRequest = async (req, res) => {
+  if (!req.params.id || !req.query.w || !req.query.h) {
+    res.sendStatus(400);
+    return;
+  }
+  const safeId = encodeURIComponent(req.params.id);
+  const safeW = encodeURIComponent(req.query.w);
+  const safeH = encodeURIComponent(req.query.h);
+  const url = `https://docs.google.com/drawings/d/e/${safeId}/pub?w=${safeW}&h=${safeH}`;
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'user-agent': req.headers['user-agent'],
+      },
+    });
 
-const handleRequest = (req, res) => {
-  var options = {
-    url:
-      'https://docs.google.com/drawings/d/e/' +
-      req.params.id +
-      '/pub?w=' +
-      req.query.w +
-      '&h=' +
-      req.query.h,
-    headers: {
-      'User-Agent': req.headers['user-agent'],
-    },
-    encoding: null,
-  };
-  request(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      res.set('Content-Type', 'image/png').send(body);
+    if (response.ok && response.headers.get('content-type') == 'image/png') {
+      const data = await response.bytes();
+      res.set('content-type', 'image/png').send(data);
     } else {
-      console.error('[GDrawings] Request error: ' + JSON.stringify(error));
       res.sendStatus(500);
+      return;
     }
-  });
+  } catch (error) {
+    console.error('[GDrawings] Request Error', error);
+    res.sendStatus(502);
+    return;
+  }
 };
 
 export default handleRequest;
